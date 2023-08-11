72c8b20aacb70334e12b6c8fb4bccbbcf6472e9d
bhavik712 <baradbhavik41@gmail.com>
2023-08-11 19:25:39 +0530

    testing

---
+++ src/App.vue
@@ -0,0 +1,24 @@
+<template>
+  <form-input :fieldProperties="ageField"></form-input>
+</template>
+
+<script>
+import { FormInput } from "vue-component-lib-bhavik";
+import "vue-component-lib-bhavik/style.css";
+export default {
+  name: "app",
+  components: {
+    FormInput,
+  },
+  data() {
+    return {
+      ageField: {
+        label: "Age",
+        name: "userAge",
+        type: "number",
+        placeholder: "Enter Your Age",
+      },
+    };
+  },
+};
+</script>
